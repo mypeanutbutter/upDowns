9407512ed060e94d7ea8fbb6d0f28e3477091c93
mypeanutbutter <peanutbutter@tuta.io>
2015-11-22 21:25:20 +0200

    Updated updown.py
    Added scan timer, changed some names, added some comments, added
    exceptions and some more stuff I can't remember.

--- upDown/updown.py
+++ upDown/updown.py
@@ -2,77 +2,119 @@
 # -*- coding: UTF-8 -*
 from sys import argv
 import sys
+import time
 import requests
 import linecache
 from urllib2 import Request, urlopen, HTTPError, URLError
+import httplib
+from timeit import default_timer as timer
 from colorama import Fore, Back, Style
 from os.path import exists
 
-script, mode, target = argv
-
-print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
-print("   Check for online websites, by peanutButter")
-print("       Peanut Butter is tasty. yum.")
-print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
-
+## Variables for counting
 up = 0 # Variable for amount of websites that are up in the scan
 down = 0 # Variable for amount of websites that are down in the scan
-cloudflare = 0 # Variable for amount of websites that are potentially protected by cloudflare (Error 403)
+crimeflare = 0 # Variable for amount of websites that are potentially protected by cloudflare (Error 403)
 
-if len(sys.argv) == 1 or len(sys.argv) == 2: # If there's less than 3 arugments
+sprinklyline = "~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~" # Cus why not
+
+## Welcome message
+print sprinklyline
+print("   Check for online websites, by peanutButter")
+print("       Peanut Butter is tasty. yum.")
+print sprinklyline
+
+def scancomplete(): # Didn't let me use back / fore colors with """ so we have to use multiple print lines :c
+	print sprinklyline
+	print(Back.GREEN + Fore.WHITE +"       Scan complete "+Style.RESET_ALL+", Results are above!")
+	print sprinklyline	
+
+if len(sys.argv) < 3: # If there's less than 3 arugments, ex: updown.py -t and no textfile selected / updown.py and no mode or textfile/ domain selected
 	print(Fore.WHITE + "No options selected, Examples can be seen below:" + Style.RESET_ALL)
-	print "	Options:"
-	print "		-d Domain -> Check a domain, Example: 'python updown.py -d example.com'"
-	print "		-t Text file -> Check a text file with domains (1 Domain in a line), Example: 'python updown.py -t websitelist.txt'"
+	print "Avaliable options for use:"
+	print("      "+ Fore.WHITE + Back.GREEN + "-d" + Style.RESET_ALL + " Domain -> For single website checks")
+	print("      "+ Fore.WHITE + Back.GREEN + "-t" + Style.RESET_ALL + " Textfile -> For a textfile with a bunch of websites")
+	print sprinklyline
+
 else:
+	script, mode, target = argv
 	if mode == '-d': # If mode -d selected, domain check.
+		
+		## Timer
+		start = timer() # Start scan timer
+	
 		user_agent = 'Mozilla/20.0.1 (compatible; MSIE 5.5; Windows NT)' # User agent settings
 		headers = { 'User-Agent':user_agent } # Headers
 		req = Request(target, headers = headers) # Generate request
 
+		## Single website check:
 		try: # Try opening the target page
 			page_open = urlopen(req)
 		except HTTPError, e: # If there's an HTTP
-			print(Fore.WHITE + target + Fore.RED + u"\u2718" +" Is down! Error recieved : %s, %s" + Style.RESET_ALL) % (e.code, e.reason)
-			down += 1
+			if e.code==403: # Check for error 403, mark as cloudflare and up
+				print(Fore.WHITE + target + Fore.GREEN + u"\u2713" +" Is up! (Cloudflare?)" + Style.RESET_ALL)
+				crimeflare += 1
+				up += 1
+			else: #Anything other than 403 = down
+				print(Fore.WHITE + target + Fore.RED + u"\u2718" +" Is down! Error recieved : %s, %s" + Style.RESET_ALL) % (e.code, e.reason)
+				down += 1
 		except URLError, e: # If there's an URL error
-			print(Fore.WHITE + target + Fore.RED + u"\u2718" +" Is down! URL doesn't exist? : %s" + Style.RESET_ALL) % (e.reason)
+			print(Fore.WHITE + target + Fore.RED + u"\u2718" +" Is down! : %s" + Style.RESET_ALL) % (e.reason)
 			down += 1
+		except httplib.HTTPException, e:
+   			print(Fore.WHITE + target + Fore.RED + u"\u2718" +" Not sure? HTTP Exception" + Style.RESET_ALL)
+		except Exception:
+    			import traceback
+    			print(Fore.YELLOW + "Generic exception while checking: " + Fore.WHITE + line + Fore.YELLOW + "(Connection reset by peer)" + Style.RESET_ALL)
 		else: # If its up and all goes well
         		print(Fore.WHITE + target + Fore.GREEN + u"\u2713" +" Is up!" + Style.RESET_ALL)
 			up += 1
-
-		print "Number of websites down: ", down
-		print "Number of websites up: ", up
-		print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
-		print(Back.GREEN + Fore.WHITE +"       Scan complete "+Style.RESET_ALL+", Results are above!")
-		print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
+		print sprinklyline
+		## Timer
+		end = timer() # Stop scan timer
+		timescale = 'seconds' # Set default scan timescale to seconds
+		timerresult = end - start # Calculate time taken to complete scan
+		if timerresult > 60: # Check if scan takes more than 60 seconds
+			newtimerresult = timerresult / 60 # Div by 60 to get minutes
+			timescale = 'minutes' # Change scan timescale to minutes, because we calculated minutes.
+		else: # If scan doesn't take more than 60 seconds
+			newtimerresult = timerresult # Change timer result to the original timer result.
+		newtimerresult = int(newtimerresult) # Remove numbers after the decimal point
+		print "Time taken to complete scan: %s %s" % (newtimerresult, timescale) # Print timer result
+		print "Tangos down: ", down
+		print "Up and running: ", up
+		print "Potentially Cloudflare'd: ", crimeflare, newtimerresult
+		
+		scancomplete() # Prints scan complete text
 
 	if mode == '-t': # If mode -t selected, textlist check.
 		if bool(exists(target)): # Check if the textfile set as target actually exists
 			txt = open(target) # Open text file specified
 			print (Fore.WHITE + "File information:" + Style.RESET_ALL)
-			print "Text file selected: ", target 
+			print "Text file selected: ", target # Prints the text file selected
 
-			countlines = max(enumerate(open(target)))[0] # Returns number of lines - 1
-			lines = countlines + 1
+			countlines = max(enumerate(open(target)))[0] # Returns number of lines minus 1
+			lines = countlines + 1 # Add 1 to the line result, because ^
 
-			print "Number of websites: ", lines
-			print "-----------------------------"
+			print "Number of websites: ", lines # Returns number of websites in the text file (lines)
+			print sprinklyline
 			print (Fore.WHITE + "Scan process:" + Style.RESET_ALL)
+			print sprinklyline
+			
+			start = timer() # Start clock
 
-			clinenum = 0
+			clinenum = 0 # Current line number
 			while (clinenum<lines):	
 				user_agent = 'Mozilla/20.0.1 (compatible; MSIE 5.5; Windows NT)' # User agent settings
 				headers = { 'User-Agent':user_agent } # Headers
-				line = open(target).readlines()[clinenum]
+				line = open(target).readlines()[clinenum] # Open target and select line
 				req = Request(line, headers = headers) # Generate request
 				try: # Try opening the target page
 					page_open = urlopen(req)
 				except HTTPError, e: # If there's an HTTP
 					if e.code==403: # Check for error 403, mark as cloudflare and up
 						print(Fore.WHITE + line + Fore.GREEN + u"\u2713" +" Is up! (Cloudflare?)" + Style.RESET_ALL)
-						cloudflare += 1
+						crimeflare += 1
 						up += 1
 					else: #Anything other than 403 = down
 						print(Fore.WHITE + line + Fore.RED + u"\u2718" +" Is down! Error recieved : %s, %s" + Style.RESET_ALL) % (e.code, e.reason)
@@ -80,27 +122,33 @@ else:
 				except URLError, e: # If there's an URL error
 					print(Fore.WHITE + line + Fore.RED + u"\u2718" +" Is down! : %s" + Style.RESET_ALL) % (e.reason)
 					down += 1
+				except httplib.HTTPException, e:
+   					print(Fore.WHITE + line + Fore.RED + u"\u2718" +" Not sure? HTTP Exception" + Style.RESET_ALL)
+				except Exception:
+    					import traceback
+    					print(Fore.YELLOW + "Generic exception while checking: " + Fore.WHITE + line + Fore.YELLOW + "(Connection reset by peer)" + Style.RESET_ALL)
 				else: # If its up and all goes well
         				print(Fore.WHITE + line + Fore.GREEN + u"\u2713" +" Is up!" + Style.RESET_ALL)
 					up += 1
+				print " " # Empty line for style
 				clinenum += 1
 
-			print "Number of websites down: ", down
-			print "Number of websites up: ", up
-			print "Number of websites potentially protected by Cloudflare: ", cloudflare
-			print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
-			print(Back.GREEN + Fore.WHITE +"       Scan complete "+Style.RESET_ALL+", Results are above!")
-			print("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~")
+			print sprinklyline
+			end = timer() # Stop scan timer
+			timescale = 'seconds' # Set default scan timescale to seconds
+			timerresult = end - start # Calculate time taken to complete scan
+			if timerresult > 60: # Check if scan takes more than 60 seconds
+				newtimerresult = timerresult / 60 # Div by 60 to get minutes
+				timescale = 'minutes' # Change scan timescale to minutes, because we calculated minutes.
+			else: # If scan doesn't take more than 60 seconds
+				newtimerresult = timerresult # Change timer result to the original timer result.
+			newtimerresult = int(newtimerresult) # Remove numbers after the decimal point
+			print "Time taken to complete scan: %s %s" % (newtimerresult, timescale) # Print timer result
+			print "Tangos down: ", down
+			print "Up and running: ", up
+			print "Potentially Cloudflare'd: ", crimeflare
 
-		else:
+			scancomplete() # Prints scan completed text
+
+		else: # If the textfile provided doesn't exist
 			print (Fore.WHITE + "The text file you set as target doesn't exist, wrong path?" + Style.RESET_ALL)
-
-
-#if len(sys.argv)==1:
-#	print "Invalid domain"
-#else:
-#	try: 
-#		urllib.urlopen('http://' + sys.argv[1]) 
-#		print(Fore.WHITE + sys.argv[1] + Fore.GREEN + u"\u2713" +" Is up!")
-#	except:
-#   		print(Fore.RED + u"\u2718" +" Down")
